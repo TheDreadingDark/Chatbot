7cc1eb8bd3e78701c4bedffce58e8286fe87b762
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-13 13:44:23 -0700

    Added code for setupFrame and ChatFrame.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,23 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatbotController appController;
+	private ChatPanel appPane;
+	
+	public ChatFrame(ChatbobtController appController)
+	{
+		super();
+		this.appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 }
