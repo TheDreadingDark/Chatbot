0a3f74cafbd24e72a045940b6fd199849b2e33de
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2017-11-15 13:06:35 -0700

    Changed background color and aligned the chat panel.

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,57 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		//Initialize GUI data members
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10, 25);
+		inputField = new JTextField(20);
+		appLayout = new SpringLayout();
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.GREEN);
+		this.setLayout(appLayout);
+		this.add(chatButton);
+		this.add(inputField);
+		this.add(chatArea);
+	}
+	
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, chatArea, 20, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, chatArea, 25, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.EAST, chatArea, -25, SpringLayout.EAST, this);
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
