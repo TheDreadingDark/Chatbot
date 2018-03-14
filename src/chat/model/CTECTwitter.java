a2ae349b10d69bd61b074cbd22790328096d80e4
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-03-14 13:54:37 -0600

    Added sortedWords method.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -81,7 +81,44 @@ public class CTECTwitter
 		(DecimalFormat.getPercentInstance().format(((double) maxWord)/wordsAndCount.size())) + 
 		" of the unique words: " + wordsAndCount.size();
 		
+		mostCommon += "\n\n" + sortedWords();
+		
 		return mostCommon;
+	}
+	
+	private String sortedWords()
+	{
+		String allWords = "";
+		String [] words = new String [wordsAndCount.size()];
+		ArrayList<String> wordList = new ArrayList<String>(wordsAndCount.keySet());
+		
+		for(int index = 0; index < wordsAndCount.size(); index++)
+		{
+			words[index] = wordList.get(index);
+		}
+		for(int = 0; index < words.length - 1; index++)
+		{
+			int maxIndex = index;
+			
+			for (int inner = index + 1; inner < words.length; inner++)
+			{
+				if (words[inner].compareTo(words[maxIndex]) > 0)
+				{
+					maxIndex = inner;
+				}
+			}
+			
+			String tempMax = words[maxIndex];
+			words[maxIndex] = words[index];
+			words[index] = tempMax;
+		}
+		
+		for (String word : words)
+		{
+			allWords += word + ", ";
+		}
+		
+		return allWords;
 	}
 	
 	private void collectTweets(String username)
